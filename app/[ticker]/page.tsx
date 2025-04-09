2a7d5384b6216aca87a59b7588d011fb9c46b001
Korolev Fedor <ext-fedor.korolev@sbermarket.ru>
2025-04-09 12:11:11 +0200

    fix getQueryClient

--- app/[ticker]/page.tsx
+++ app/[ticker]/page.tsx
@@ -1,20 +1,17 @@
-import {
-  dehydrate,
-  HydrationBoundary,
-  QueryClient,
-} from "@tanstack/react-query";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 import { getExtendedRatesQueryOptions } from "@/src/queries/useExtendedRatesSuspenseQuery/getExtendedRatesQueryOptions";
 import { CriptoDetails } from "@/src/features/CryptocurrencyDetails/block/CriptoDetails/CriptoDetails.client";
 import styles from "./page.module.css";
 import { CryptocurrencyDetailsPageParams } from "./page.types";
 import { getExtraCurrencyDetailsQueryOptions } from "@/src/queries/useExtraCurrencyDetailsSuspenseQuery/getExtraCurrencyDetailsQueryOptions";
 import { Breadcrumbs } from "@/src/common/components/Breadcrumbs/Breadcrumbs.client";
+import { getQueryClient } from "@/src/common/providers/QueryInitializeProvider/utils/getQueryClient";
 
 export default async function CryptocurrencyDetailsPage({
   params,
 }: CryptocurrencyDetailsPageParams) {
   const { ticker } = await params;
-  const queryClient = new QueryClient();
+  const queryClient = getQueryClient();
 
   await Promise.all([
     queryClient.prefetchQuery(getExtendedRatesQueryOptions()),
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,11 @@
-import {
-  dehydrate,
-  HydrationBoundary,
-  QueryClient,
-} from "@tanstack/react-query";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 import { getExtendedRatesQueryOptions } from "@/src/queries/useExtendedRatesSuspenseQuery/getExtendedRatesQueryOptions";
 import { CryptoList } from "@/src/features/CryptocurrencyList/block/CryptoList/CryptoList.client";
 import { isBotOnlyServer } from "@/src/common/utils/isBotOnlyServer";
+import { getQueryClient } from "@/src/common/providers/QueryInitializeProvider/utils/getQueryClient";
 
 export default async function Home() {
-  const queryClient = new QueryClient();
+  const queryClient = getQueryClient();
 
   await queryClient.prefetchQuery(getExtendedRatesQueryOptions());
 
